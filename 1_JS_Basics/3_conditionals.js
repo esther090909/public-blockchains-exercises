45d7e5130c6230af4ba2c7f55f288e8146c96f1d
Esther Krystek <krystek_iis@uni-mannheim.de>
2024-03-20 00:43:41 +0100

    conditionals

--- 1_JS_Basics/3_conditionals.js
+++ 1_JS_Basics/3_conditionals.js
@@ -43,6 +43,17 @@
 // need to create a new variable with the population of Luzern here.
 // Hint2: Use console.log() to print.
 
+luzernPopulation = 8200;
+randNum = Math.random() * 100000;
+
+if (randNum > luzernPopulation) {
+    console.log('Go Luzern!');
+} else if (randNum === luzernPopulation) {
+    console.log('Are we in the Matrix?');
+} else {
+    console.log('Few but good!');
+}
+
 // EXERCISE 2. Block Party.
 ///////////////////////////
 
@@ -56,9 +67,17 @@ if (2 > 1) {
 console.log(deepThought);
 
 // a. Fix it using it var instead of let.
+if (2 > 1) {
+    var deepThought = 'Everything is obvious once you know the answer.'    
+}
+console.log(deepThought);
 
 // b. Fix it using let.
-
+let deepThought;
+if (2 > 1) {
+    deepThought = 'Everything is obvious once you know the answer.'    
+}
+console.log(deepThought);
 
 // EXERCISE 3. Loose vs Strict Comparisons.
 ///////////////////////////////////////////
