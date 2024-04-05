9fa2e44cb1ae5fcc7a9e7691ac915380528d5026
Esther Krystek <krystek_iis@uni-mannheim.de>
2024-04-05 12:56:43 +0200

    hardhatex1

--- 4_Hardhat_ex/1_hardhat.js
+++ 4_Hardhat_ex/1_hardhat.js
@@ -7,7 +7,10 @@
 // a. Require the `dotenv` and `ethers` packages.
 // Hint: As you did multiple times now.
 
-// Your code here!
+const path = require('path');
+pathToDotEnv = path.join(__dirname, '..', '.env');
+require("dotenv").config({ path: pathToDotEnv });
+const ethers = require("ethers");
 
 
 // Exercise 1. Create a JSON RPC Provider for the Hardhat blockchain.
@@ -16,21 +19,26 @@
 // Hint: you will find the info printed to console after you start the hardhat
 // blockchain.
 
-// Your code here!
+const hardhatUrl = "http://127.0.0.1:8545";
+const hardhatProvider = new ethers.JsonRpcProvider(hardhatUrl);
 
 // Exercise 2. Let's query the provider.
 ////////////////////////////////////////
 
-// Hardhat Blockchain si too long. Let's call it NUMA.
+// Hardhat Blockchain too long. Let's call it NUMA.
 // Print to console the network name, chain id, and block number of NUMA.
 
 const networkInfo = async () => {
-   
-    // Your code here!
+    let net = await hardhatProvider.getNetwork();
+    console.log('NUMA Info:');
+    console.log('Network name: ', net.name);
+    console.log('Network chain id: ', Number(net.chainId));
+
+    let blockNumber = await hardhatProvider.getBlockNumber();
+    console.log('Block number: ', blockNumber);
 };
 
-// networkInfo();
-
+networkInfo();
 
 
 // Exercise 3. Signer on the Hardhat blockchain.
@@ -40,24 +48,28 @@ const networkInfo = async () => {
 // the Hardhat blockchain.
 // Hint: check the Hardhat console output.
 
-// Your code here.
+let hhPrivateKey = '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80';
+let signer = new ethers.Wallet(hhPrivateKey, hardhatProvider);
+console.log(signer.address);
 
 // b. Check the balance of the signer.
 
 const checkBalance = async () => {
-    // Your code here.
+    let balance = await hardhatProvider.getBalance(signer.address);
+    console.log('My balance is ' + ethers.formatEther(balance) + ' ETH.');
 };
 
-// checkBalance();
+checkBalance();
 
 // c. Print the signer's next nonce necessary to send a transaction.
 // Hint: .getNonce()
 
 const getNonce = async() => {
-    // Your code here.
+    let nonce = await signer.getNonce();
+    console.log('Your nonce is ' + nonce);
 };
 
-// getNonce();
+getNonce();
 
 
 // Exercise 4. Send a transaction.
@@ -67,12 +79,30 @@ const getNonce = async() => {
 // accounts on Metamask (e.g., the one used to make the submissions in 
 // this course).
 
-const account2 = process.env.METAMASK_2_ADDRESS;
+const account2 = '0x70997970C51812dc3A010C7d01b50e0d17dc79C8';
 
 const sendTransaction = async () => {
+    let b1 = await hardhatProvider.getBalance(signer.address);
+    let b2 = await hardhatProvider.getBalance(account2);
+    b1 = ethers.formatEther(b1);
+    b2 = ethers.formatEther(b2);
 
-    // Your code here!
+    tx = await signer.sendTransaction({
+        to: account2,
+        value: ethers.parseEther("0.01")
+    });
+    console.log('Transaction is in the mempool...');
+    await tx.wait();
+    
+    console.log('Transaction mined!');
+    
+    let updatedB1 = await hardhatProvider.getBalance(signer.address);
+    let updatedB2 = await hardhatProvider.getBalance(account2);
+    updatedB1 = ethers.formatEther(updatedB1);
+    updatedB2 = ethers.formatEther(updatedB2);
+
+    console.log('Balance for', signer.address, 'changed from', b1, 'to', updatedB1);
+    console.log('Balance for', account2, 'changed from', b2, 'to', updatedB2);
 };
 
-// sendTransaction();
-
+sendTransaction();
